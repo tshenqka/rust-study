30a48427d7d1b94ec2442259da1b348d4e22315f
tshenqka <66680777+tshenqka@users.noreply.github.com>
2024-06-22 00:39:11 -0400

    finish chapter 12.3 on refactoring improvements and use of library

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,24 +1,25 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    let config = Config::build(&args).unwrap_or_else(|err| { // between vertical pipes
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Applications error: {e}");
+        process::exit(1);
+    }
 }
 
-struct Config {
-    query: String,
-    file_path: String
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config {query, file_path}
-}
 
 // // old version
 // fn main() {
