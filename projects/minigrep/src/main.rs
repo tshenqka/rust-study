3866d4820afe163e25284141ab9eb8b1ade9130e
tshenqka <66680777+tshenqka@users.noreply.github.com>
2024-06-22 18:51:40 -0400

    finish chapter 12

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -7,12 +7,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| { // between vertical pipes
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Applications error: {e}");
+        eprintln!("Applications error: {e}");
         process::exit(1);
     }
 }
