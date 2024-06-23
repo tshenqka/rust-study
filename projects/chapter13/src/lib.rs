be3796a022521ca52daca182494e609c2af4294d
tshenqka <66680777+tshenqka@users.noreply.github.com>
2024-06-22 22:57:49 -0400

    finish chapter 13.2

---
+++ projects/chapter13/src/lib.rs
@@ -0,0 +1,29 @@
+#[test]
+fn iterator_demonstration() {
+    let v1 = vec![1, 2, 3];
+
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
+
+
+[tests]
+fn iterator_demonstration {
+    let mut v1 = vec![1, 2, 3]; 
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
+
+        let mut v1_iter = v1.iter();
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
+
+}
