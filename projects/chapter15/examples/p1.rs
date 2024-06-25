8c65f74396c283feb32b38e3bd6c117f7847b520
tshenqka <66680777+tshenqka@users.noreply.github.com>
2024-06-24 23:55:55 -0400

    finish chapter 15.2

--- projects/chapter15/examples/p1.rs
+++ projects/chapter15/examples/p1.rs
@@ -3,12 +3,41 @@
 //     Nil,
 // } // in this example we get an error suggesting indirection: store a pointer to the value instead
 
+use std::ops::Deref;
+
 
 enum List {
     Cons(i32, Box<List>),
     Nil,
 }
 
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
 fn main() {
-    return;
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
