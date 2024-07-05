db1f0dac12e17b47f664699f1cfc52f564231fee
tshenqka <66680777+tshenqka@users.noreply.github.com>
2024-07-04 23:54:11 -0400

    finish chapter 17 blog project

---
+++ projects/blog/src/main.rs
@@ -0,0 +1,17 @@
+
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
+
+// rust can do oop but encoding state into type system is also alternative and have different trade-offs
+// take advantage of rust features like ownership
