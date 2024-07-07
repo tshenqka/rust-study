f05444a26332875ef04b111863f861d088ccd909
tshenqka <66680777+tshenqka@users.noreply.github.com>
2024-07-07 18:45:50 -0400

    finish chapter 20.3 on multi threaded servers

--- projects/hello/src/main.rs
+++ projects/hello/src/main.rs
@@ -7,14 +7,17 @@ use std::{
 
 };
 
+use hello::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        thread::spawn(|| {
-            handle_connection(stream); // create new thead each time
-        });
+        pool.execute(|| {
+            handle_connection(stream); // pass by value since tcp is not send
+        })
     }
 }
 
